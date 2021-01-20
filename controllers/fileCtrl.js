2fc7e0bfe3d2bf7277b78be638ee8e6c1cca58d3
Nitzan Maizel <nitzanmaizel@gmail.com>
2021-01-20 22:12:35 +0200

    Chat page

--- controllers/fileCtrl.js
+++ controllers/fileCtrl.js
@@ -1,105 +1,116 @@
-const CaseFile = require("../models/CaseFile");
-const User = require("../models/User");
-const cloudinary = require("cloudinary");
+const CaseFile = require('../models/CaseFile');
+const User = require('../models/User');
+const cloudinary = require('cloudinary');
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CONFIG_NAME,
-  api_key: process.env.CLOUDINARY_CONFIG_KEY,
-  api_secret: process.env.CLOUDINARY_CONFIG_SECRET,
+	cloud_name: process.env.CLOUDINARY_CONFIG_NAME,
+	api_key: process.env.CLOUDINARY_CONFIG_KEY,
+	api_secret: process.env.CLOUDINARY_CONFIG_SECRET,
 });
 
 const UploadFile = async (req, res) => {
-  let cloud = {};
-  if (req.file) {
-    cloud = await cloudinary.v2.uploader.upload(req.file.path).catch((err) => {
-      return res.status(500).json("Error uploading photo");
-    });
-  } else {
-    return res.status(400).json("No file");
-  }
-  if (!req.body.patientCaseFileId) {
-    return res.status(400).json("Missing Information");
-  }
-  const newFile = new CaseFile({
-    user_id: req.user._id,
-    patient_case_file_id: req.body.patientCaseFileId,
-    age: req.body.age,
-    gender: req.body.gender,
-    localization: req.body.localization,
-    status: req.body.status,
-    diameter: req.body.diameter,
-    evolution: req.body.evolution,
-    history: req.body.history,
-    image_url: cloud.url,
-  });
+	let cloud = {};
+	if (req.file) {
+		cloud = await cloudinary.v2.uploader.upload(req.file.path).catch((err) => {
+			return res.status(500).json('Error uploading photo');
+		});
+	} else {
+		return res.status(400).json('No file');
+	}
+	if (!req.body.patientCaseFileId) {
+		return res.status(400).json('Missing Information');
+	}
+	const newFile = new CaseFile({
+		user_id: req.user._id,
+		patient_case_file_id: req.body.patientCaseFileId,
+		age: req.body.age,
+		gender: req.body.gender,
+		localization: req.body.localization,
+		status: req.body.status,
+		diameter: req.body.diameter,
+		evolution: req.body.evolution,
+		history: req.body.history,
+		image_url: cloud.url,
+	});
 
-  newFile.save().catch((err) => {
-    return res.status(500).json("Server Error");
-  });
-  User.findOne({ _id: req.user._id }).then((user) => {
-    if (!user) {
-      return res.status(500).json("Server Error!");
-    }
-    user.case_files.push(newFile._id);
-    user
-      .save()
-      .then((user) => {
-        res.json({
-          user,
-        });
-      })
-      .catch((err) => {
-        return res.status(500).json("Server Error");
-      });
-  });
+	newFile.save().catch((err) => {
+		return res.status(500).json('Server Error');
+	});
+	User.findOne({ _id: req.user._id }).then((user) => {
+		if (!user) {
+			return res.status(500).json('Server Error!');
+		}
+		user.case_files.push(newFile._id);
+		user
+			.save()
+			.then((user) => {
+				res.json({
+					user,
+				});
+			})
+			.catch((err) => {
+				return res.status(500).json('Server Error');
+			});
+	});
 };
 
 const PostCommentFile = async (req, res) => {
-  if (!req.body.file_id || !req.body.comment_content) {
-    return res.status(400).json("Missing Information");
-  }
-  CaseFile.findOne({ _id: req.body.file_id }).then((casefile) => {
-    const newComment = {
-      user_id: req.user._id,
-      date: Date.now(),
-      comment: req.body.comment_content,
-    };
-    casefile.comments.push(newComment);
-    casefile
-      .save()
-      .then((newcasefile) => {
-        return res.json(newcasefile);
-      })
-      .catch((err) => {
-        return res.status(500).json("Server Error");
-      });
-  });
+	if (!req.body.file_id || !req.body.comment_content) {
+		return res.status(400).json('Missing Information');
+	}
+	CaseFile.findOne({ _id: req.body.file_id }).then((casefile) => {
+		const newComment = {
+			user_id: req.user._id,
+			date: Date.now(),
+			comment: req.body.comment_content,
+		};
+		casefile.comments.push(newComment);
+		casefile
+			.save()
+			.then((newcasefile) => {
+				return res.json(newcasefile);
+			})
+			.catch((err) => {
+				return res.status(500).json('Server Error');
+			});
+	});
 };
 
 const PostSurveyFile = async (req, res) => {
-  if (!req.body.file_id || !req.body.survey_content) {
-    return res.status(400).json("Missing Information");
-  }
-  CaseFile.findOne({ _id: req.body.file_id }).then((casefile) => {
-    const newSurvey = {
-      user_id: req.user._id,
-      date: Date.now(),
-      comment: req.body.survey_content,
-    };
-    casefile.survey.push(newSurvey);
-    casefile
-      .save()
-      .then((newcasefile) => {
-        return res.json(newcasefile);
-      })
-      .catch((err) => {
-        return res.status(500).json("Server Error");
-      });
-  });
+	if (!req.body.file_id || !req.body.survey_content) {
+		return res.status(400).json('Missing Information');
+	}
+	CaseFile.findOne({ _id: req.body.file_id }).then((casefile) => {
+		const newSurvey = {
+			user_id: req.user._id,
+			date: Date.now(),
+			comment: req.body.survey_content,
+		};
+		casefile.survey.push(newSurvey);
+		casefile
+			.save()
+			.then((newcasefile) => {
+				return res.json(newcasefile);
+			})
+			.catch((err) => {
+				return res.status(500).json('Server Error');
+			});
+	});
+};
+
+const GetFile = async (req, res) => {
+	CaseFile.findOne({ _id: req.params.id })
+		.then((file) => {
+			return res.json(file);
+		})
+		.catch((err) => {
+			res.status(500).send('Server Error');
+		});
 };
 
 module.exports = {
-  UploadFile: UploadFile,
-  PostCommentFile: PostCommentFile,
-  PostSurveyFile: PostSurveyFile,
+	UploadFile: UploadFile,
+	PostCommentFile: PostCommentFile,
+	PostSurveyFile: PostSurveyFile,
+	GetFile: GetFile,
 };
--- routes/files.js
+++ routes/files.js
@@ -1,31 +1,25 @@
-const upload = require("../config/multer");
-const express = require("express");
+const upload = require('../config/multer');
+const express = require('express');
 const router = express.Router();
-const passport = require("passport");
+const passport = require('passport');
 
 const {
-  UploadFile,
-  PostCommentFile,
-  PostSurveyFile,
-} = require("../controllers/fileCtrl");
+	UploadFile,
+	PostCommentFile,
+	PostSurveyFile,
+	GetFile,
+} = require('../controllers/fileCtrl');
 
 router.post(
-  "/upload",
-  upload.single("image"),
-  passport.authenticate("jwt", { session: false }),
-  UploadFile
+	'/upload',
+	upload.single('image'),
+	passport.authenticate('jwt', { session: false }),
+	UploadFile
 );
 
-router.put(
-  "/comment",
-  passport.authenticate("jwt", { session: false }),
-  PostCommentFile
-);
+router.put('/comment', passport.authenticate('jwt', { session: false }), PostCommentFile);
 
-router.put(
-  "/survey",
-  passport.authenticate("jwt", { session: false }),
-  PostSurveyFile
-);
+router.put('/survey', passport.authenticate('jwt', { session: false }), PostSurveyFile);
+router.get('/getfile/:id', passport.authenticate('jwt', { session: false }), GetFile);
 
 module.exports = router;
